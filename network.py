88bb216b7768e04a4a9b960686920421885ad620
AndrewVCoding <andrew@txvaughn.com>
2018-07-20 18:39:48 -0500

    Initial commit

---
+++ network.py
@@ -0,0 +1,53 @@
+import numpy as np
+import random
+import math
+
+
+class Netowrk:
+
+    def __init__(self, input_neurons=10, hidden_neurons=20, output_neurons=10, axon_density=0.2):
+        self.input_neurons = input_neurons
+        self.hidden_neurons = hidden_neurons
+        self.output_neurons = output_neurons
+
+        self.neuron = np.zeros(input_neurons + hidden_neurons + output_neurons) - 2.0
+
+        self.threshold = 20.0
+
+        self.resting_potential = 0.0
+
+        self.activation = self.neuron.copy()
+
+        self.state = np.array([self.neuron, self.threshold, self.resting_potential, self.activation])
+
+        self.history = [0.0]
+
+        self.return_rates = 1.1
+
+    def activate(self, activation):
+        self.neuron[0, :] = activation
+
+    def step(self, activation):
+
+        x = self.neuron
+
+        # decay
+        # The decay term to bring the potential back towards 0.0
+        d = 0.5 / np.exp(10 * np.power(x - self.resting_potential, 2) / 1000)
+
+        # The action potential fire term to reduce the potential back towards rest
+        f = 1.1 / (np.exp(50 * (x - self.threshold - 1.0)) + 1.0) - 0.6
+
+        # Apply the decay rate to the network potential
+        s = (f + d) * x
+
+        # Calculate the effect of the action potential based on the current potential
+        # The resting term 1/(1+e^(-10x))
+        r = 1.0 / (1.0 + np.exp(-10.0 * (s + 0.0)))
+
+        # The threshold term 2/(1+e^(-10x + 190))
+        n = 1.0 / (1.0 + np.exp(-1.0 * (s - 22.0)))
+
+        self.neuron = s + (n + r) * activation
+        self.state = np.array([self.neuron, self.threshold, self.resting_potential, self.activation])
+        self.history.append(self.neuron[0])
---
+++ spike.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import time
+import network
+import numpy as np
+import math
+
+
+def time_trial():
+    x = []
+    times = []
+
+    start = time.time()
+    for i in range(900, 1000):
+        trial = []
+        x.append(i)
+        t1 = time.time()
+        for n in range(0, 50):
+            trial = []
+            net = network.Netowrk(i)
+            t1 = time.time()
+            for t in range(0, 100):
+                activation = 10.0 if 0.1 * math.sin(t / 20) > 0.099 else 0.0
+                net.step(activation)
+            t2 = time.time()
+            trial.append(t2 - t1)
+        times.append(np.mean(trial))
+        print(i, ' neurons simulated in ', time.time() - t1, ' seconds')
+    print('Total Time: ', time.time() - start)
+
+    fig, ax = plt.subplots(figsize=(12, 7))
+    ax.scatter(x, times)
+    ax.set_xlabel('neurons')
+    ax.set_ylabel('avg time (seconds)')
+    ax.set_title('Time per 100 Simulation steps averaged across 50 trials')
+    plt.grid()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    np.set_printoptions(precision=2)
+
+    nn = network.Netowrk()
+
+    timesteps = [0.0]
+    activations = [0.0]
+    for t in range(0, 1000):
+        activation = 12.0 if 0.1 * math.sin(t / 20) > 0.099 else 0.0
+        nn.step(activation)
+        activations.append(activation)
+        timesteps.append(t)
+
+    input_layer = nn.history
+
+    plt.subplot(2, 1, 1)
+    plt.plot(timesteps, input_layer)
+    plt.xlabel('time step')
+    plt.ylabel('activation')
+    plt.title('Input Layer Stimulus')
+    plt.plot(timesteps, activations)
+
+    plt.grid()
+
+    plt.show()
