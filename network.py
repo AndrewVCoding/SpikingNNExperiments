0bfddb6f45903377c50c0ce2b0db8d107b176523
AndrewVCoding <andrew@txvaughn.com>
2018-07-24 14:12:47 -0500

    Graded Potentials are working

--- network.py
+++ network.py
@@ -11,28 +11,45 @@ class Network:
         np.random.seed(0)
 
         self.t = 0
+        self.graded_potentials = []
+        self.length = np.linspace(0, 100, 1000)
+
         self.Na = 50
         self.NaGates = 0.0
         self.K = 50
         self.KGates = 0.0
         self.resting_potential = -60.0
-        self.graded_potentials = [[0, 0, 0]]
         self.neuron = 0
+        self.timeStepSize = 1.0
 
-    def graded_potential(self, magnitude, width, position, x):
-        p = (magnitude / (math.pow(2*math.pi, 0.5))) * math.exp(-(1 / 2) * math.pow((x - position) / width, 2))
-        return p
+    def graded_potential(self, magnitude=1.0, width=1.0, position=0.0, x=0.0):
+        gp = (2.5 * magnitude / (math.pow(2*math.pi, 0.5))) * math.exp(-(1 / 2) * math.pow((x - position) / width, 2))
+        return gp
+
+    def graded_sum(self, p):
+        s = 0.0
+        for gp in self.graded_potentials:
+            s += self.graded_potential(magnitude=gp[0], position=gp[1], x=p)
+
+        return s
+
+    def graph(self):
+        graph = []
+
+        for p in self.length:
+            graph.append(self.graded_sum(p=p))
+
+        return graph
 
     def pump(self):
-        voltage = (self.Na + self.K)
+        voltage = 0.1 * (self.Na + self.K)
 
     def state(self):
         return self.t, self.neuron, self.Na, self.NaGates, self.K, self.KGates, self.resting_potential
 
-    def step(self, input, t):
-        self.t = t
-        self.graded_potentials[:, 2] += t
-        self.graded_potentials.append(input)
-
-        return self.state()
+    def step(self):
+        self.t += self.timeStepSize
+        for gp in self.graded_potentials:
+            gp[0] = gp[0] * 0.95
+            gp[1] += self.timeStepSize
 
--- spike.py
+++ spike.py
@@ -1,63 +1,37 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 import time
-
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.collections import PolyCollection
-from matplotlib import colors as mcolors
 
 import network
 import numpy as np
-import math
 
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+nn = network.Network()
 
-def time_trial():
-    x = []
-    times = []
-
-    start = time.time()
-    for i in range(900, 1000):
-        trial = []
-        x.append(i)
-        t1 = time.time()
-        for n in range(0, 50):
-            trial = []
-            net = network.Network(i)
-            t1 = time.time()
-            for t in range(0, 100):
-                activation = 10.0 if 0.1 * math.sin(t / 20) > 0.099 else 0.0
-                net.step(activation)
-            t2 = time.time()
-            trial.append(t2 - t1)
-        times.append(np.mean(trial))
-        print(i, ' neurons simulated in ', time.time() - t1, ' seconds')
-    print('Total Time: ', time.time() - start)
-
-    fig, ax = plt.subplots(figsize=(12, 7))
-    ax.scatter(x, times)
-    ax.set_xlabel('neurons')
-    ax.set_ylabel('avg time (seconds)')
-    ax.set_title('Time per 100 Simulation steps averaged across 50 trials')
-    plt.grid()
-
-    plt.show()
+def animate(i):
+    xs = nn.length
+    ys = nn.graph()
+    ax.clear()
+    ax.plot(xs, ys)
+    nn.step()
 
 
 if __name__ == '__main__':
     np.set_printoptions(precision=2)
+    style.use('fivethirtyeight')
 
-    nn = network.Network()
+    timesteps = np.linspace(0, 100, 500)
+    graph = []
 
-    timesteps = np.linspace(-10, 10, 2000)
-    activations = []
-    for t in timesteps:
-        activations.append(nn.graded_potential(2.5, 10, 10, t))
-
-    fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    ax = fig.add_subplot(111)
+    animation.FuncAnimation(fig, animate, interval=1000)
 
     # Plot the input signal
-    ax.plot(timesteps, activations)
+    for t in timesteps:
+        animate(1)
+        nn.step()
+
     #
     # N = 0
     # for N in range(0, len(nn.neuron)):
