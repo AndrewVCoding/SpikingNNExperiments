2b97bcdace88e41a850cfb752a266a9bcb628758
AndrewVCoding <andrew@txvaughn.com>
2018-07-31 17:50:06 -0500

    Resting potential is stable. Still need to model Calcium and chloride ions as well as work on the effect of graded potentials on the concentration of ions.

--- network.py
+++ network.py
@@ -10,46 +10,135 @@ class Network:
     def __init__(self, input_neurons=1, hidden_neurons=1, output_neurons=1, axon_density=0.2):
         np.random.seed(0)
 
-        self.t = 0
+        # Simulation Settings
+        self.time_since_clean = 0.0
+        self.timeStepSize = 0.1
+
+        # Neuron Properties
+        self.t = 0.0
+        self.clean_time = 25.0
+        self.mp = 0.0
+
+        # Input signals to the neuron
         self.graded_potentials = []
-        self.length = np.linspace(0, 100, 1000)
 
-        self.Na = 50
-        self.NaGates = 0.0
-        self.K = 50
-        self.KGates = 0.0
+        # Chemical Values
+        # Organic Anions are created inside the neuron
+        self.OA = 0.0
+        self.OA_max = 5.0
+        self.K = 0.0
+        self.NA = 0.0
+        self.CL = 0.0
+        self.CA = 0.0
+
+        # Diffusion Force Values
+        self.OA_diffuse = -1.0
+        self.K_diffuse = 1.0
+        self.NA_diffuse = 1.0
+        self.CL_diffuse = 1.0
+        self.CA_diffuse = 1.0
+
+        # Electrical force values
+        self.OA_electric = -1.0
+        self.K_electric = -1.0
+        self.NA_electric = 1.0
+        self.CL_electric = -1.0
+        self.CA_electric = 1.0
+
+        # Chemical Gate Values
+        self.OA_pass = 0.0
+        self.K_leak = 0.2
+        self.NA_leak = 0.2
         self.resting_potential = -60.0
-        self.neuron = 0
-        self.timeStepSize = 1.0
 
-    def graded_potential(self, magnitude=1.0, width=1.0, position=0.0, x=0.0):
-        gp = (2.5 * magnitude / (math.pow(2*math.pi, 0.5))) * math.exp(-(1 / 2) * math.pow((x - position) / width, 2))
-        return gp
+    def clean_up(self):
+        # Get rid of useless elements
+        self.graded_potentials = [gp for gp in self.graded_potentials if gp[1] < 110.0 and abs(gp[0]) > 0.01]
 
-    def graded_sum(self, p):
-        s = 0.0
-        for gp in self.graded_potentials:
-            s += self.graded_potential(magnitude=gp[0], position=gp[1], x=p)
+    def generate_oa(self):
+        self.OA += -0.1 * (self.OA - 5.0)
 
-        return s
+    def membrane_potential(self):
+        self.mp = -80 + (self.OA + self.K + self.NA)
 
-    def graph(self):
-        graph = []
+    def NaK_pump(self):
+        """
+        3 Na out for 2 K in
+        :return:
+        """
+        strength = 0.1
+        # strength = 0.9 * (self.mp - self.resting_potential)
+        self.NA -= 3 * strength
+        self.K += 2 * strength
 
-        for p in self.length:
-            graph.append(self.graded_sum(p=p))
+    def ClK_pump(self):
+        """
+        1 Cl out and 1 K out
+        :return:
+        """
+        strength = 0.01
+        self.CL -= 1 * strength
+        self.K -= 1 * strength
 
-        return graph
+    def CaNa_pump(self):
+        """
+        1 Ca out for 1 Na in
+        :return:
+        """
+        strength = 0.01
+        self.CA -= 1 * strength
+        self.NA += 1 * strength
 
-    def pump(self):
-        voltage = 0.1 * (self.Na + self.K)
+    def model_diffusion_force(self):
+        """
+        Negative values represent a force pushing ions out of the neuron
+        Positive values represent a force pushing ions into the neuron
 
-    def state(self):
-        return self.t, self.neuron, self.Na, self.NaGates, self.K, self.KGates, self.resting_potential
+        OA should be negative
+        NA should be positive at rest
+        K+ should be positive at rest, and should then match the electrical force
+        Cl should be positive at rest
+        Ca
+        :return:
+        """
+        self.OA_diffuse = -0.1 * self.OA
+        self.NA_diffuse = -0.1 * self.NA
+        self.K_diffuse = -0.1 * self.K
+        self.CL_diffuse = -0.1 * self.CL
+        self.CA_diffuse = -0.1 * self.CA
 
-    def step(self):
+    def model_electrical_force(self):
+        """
+        Negative values represent a force pushing ions out of the neuron
+        Positive values represent a force pushing ions into the neuron
+
+        :return:
+        """
+        self.OA_electric = 2 / (1 + math.exp(-0.1 * self.mp))
+        self.NA_electric = -2 / (1 + math.exp(-0.1 * self.mp))
+        self.K_electric = -0.1 * self.mp
+        self.CL_electric = 2 / (1 + math.exp(-0.1 * self.mp))
+        self.CA_electric = -2 / (1 + math.exp(-0.1 * self.mp))
+
+    def leak(self):
+        rate = 0.1
+        self.OA += self.OA_pass * (self.OA_diffuse + self.OA_electric)
+        self.K += rate * (self.K_diffuse + self.K_electric)
+        self.NA += rate * (self.NA_diffuse + self.NA_electric)
+        self.CL += rate * (self.CL_diffuse + self.CL_electric)
+        self.CA += rate * (self.CA_diffuse + self.CA_electric)
+
+    def step(self, input):
+        # Increment Time step
         self.t += self.timeStepSize
-        for gp in self.graded_potentials:
-            gp[0] = gp[0] * 0.95
-            gp[1] += self.timeStepSize
+        self.time_since_clean += self.timeStepSize
 
+        # Model the chemical levels
+        self.model_diffusion_force()
+        self.model_electrical_force()
+        self.leak()
+        self.generate_oa()
+        # self.NaK_pump()
+        self.membrane_potential()
+
+        self.mp += input
--- spike.py
+++ spike.py
@@ -1,3 +1,6 @@
+import random
+import math
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
@@ -6,54 +9,105 @@ import time
 import network
 import numpy as np
 
+simulation_speed = 2
+
 fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
+fig.subplots_adjust(wspace=1.0, hspace=1.0)
+oa_subplot = fig.add_subplot(2, 2, 2)
+mp_subplot = fig.add_subplot(2, 2, 1)
+na_subplot = fig.add_subplot(2, 2, 3)
+k_subplot = fig.add_subplot(2, 2, 4)
 nn = network.Network()
 
+window = 200
+
+t = []
+oa = []
+mp = []
+na = []
+k = []
+inputs = []
+
+
+def simulate():
+    # Retrieve the relevant information
+    t.append(nn.t)
+    if len(t) >= window:
+        t.pop(0)
+    oa.append(nn.OA)
+    if len(oa) >= window:
+        oa.pop(0)
+    mp.append(nn.mp)
+    if len(mp) >= window:
+        mp.pop(0)
+    na.append(nn.NA)
+    if len(na) >= window:
+        na.pop(0)
+    k.append(nn.K)
+    if len(k) >= window:
+        k.pop(0)
+
+    input = 20.0 * math.sin(nn.t / 1) if math.sin(nn.t / 1) > 0.999 else 0.0
+    inputs.append(input)
+    if len(inputs) >= window:
+        inputs.pop(0)
+
+    nn.step(input)
+
+
+def mp_graph():
+    xs = t
+    ys = mp
+    mp_subplot.clear()
+    mp_subplot.plot(xs, ys)
+    mp_subplot.plot(xs, inputs)
+    mp_subplot.set_ylabel('membrane potential (mV)')
+    mp_subplot.set_ylim(-80, 50)
+
+
+def oa_graph():
+    xs = t
+    ys = oa
+    oa_subplot.clear()
+    oa_subplot.plot(xs, ys)
+    oa_subplot.set_ylabel('OA')
+    oa_subplot.set_xlabel('time')
+    # oa_subplot.set_ylim(-70, 20)
+
+
+def na_graph():
+    xs = t
+    ys = na
+    na_subplot.clear()
+    na_subplot.plot(xs, ys)
+    na_subplot.set_ylabel('Na')
+    na_subplot.set_xlabel('time')
+    # na_subplot.set_ylim(-70, 20)
+
+
+def k_graph():
+    xs = t
+    ys = k
+    k_subplot.clear()
+    k_subplot.plot(xs, ys)
+    k_subplot.set_ylabel('K')
+    k_subplot.set_xlabel('time')
+    # k_subplot.set_ylim(-70, 20)
+
+
 def animate(i):
-    xs = nn.length
-    ys = nn.graph()
-    ax.clear()
-    ax.plot(xs, ys)
-    nn.step()
+    for x in range(0, simulation_speed):
+        simulate()
+    mp_graph()
+    # oa_graph()
+    na_graph()
+    k_graph()
 
 
 if __name__ == '__main__':
     np.set_printoptions(precision=2)
-    style.use('fivethirtyeight')
+    # style.use('fivethirtyeight')
 
-    timesteps = np.linspace(0, 100, 500)
-    graph = []
-
-    animation.FuncAnimation(fig, animate, interval=1000)
-
-    # Plot the input signal
-    for t in timesteps:
-        animate(1)
-        nn.step()
-
-    #
-    # N = 0
-    # for N in range(0, len(nn.neuron)):
-    #     xyz2 = []
-    #     for point in xyz:
-    #         if point[0] == N:
-    #             xyz2.append(point)
-    #
-    #     xyz2 = np.array(xyz2)
-    #     # Voltage
-    #     z = xyz2[:, 2]
-    #     # Time
-    #     y = xyz2[:, 1]
-    #     # Neuron
-    #     x = xyz2[:, 0]
-    #
-    #     # ax.plot(x, y, z)
-    #     ax.plot(y, z)
-
-    ax.set_xlabel('position')
-    ax.set_ylabel('Potential(mV)')
-    # ax.set_zlabel('POTENTIAL')
-    plt.grid()
+    ani = animation.FuncAnimation(fig, animate, interval=1)
 
     plt.show()
