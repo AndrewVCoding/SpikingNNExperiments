7412dd40d280bdc490e1936bc18c28c43503230b
AndrewVCoding <andrew@txvaughn.com>
2018-09-20 20:22:44 -0500

    Equation is a mess that causes the membrane potential to go haywire long after an input has stopped. Am going to try a different approach, using simple approximations of positive and negative ion channels.

--- network.py
+++ network.py
@@ -7,7 +7,7 @@ class Network:
 
     # @todo Design the axon functions
     # @todo Design a function to build a network of any size with the proper connections
-    def __init__(self, input_neurons=1, hidden_neurons=1, output_neurons=1, axon_density=0.2):
+    def __init__(self, input_neurons=1, hidden_neurons=0, output_neurons=0, axon_density=0.2):
         np.random.seed(2)
 
         # Record the number of neurons in the three layers
@@ -18,41 +18,45 @@ class Network:
         size = input_neurons + hidden_neurons + output_neurons
 
         # Create an ndarray of zeros to represent the potential of each neuron
-        self.neuron = 2 * (np.random.rand(size, 1) - 0.5)
+        self.neuron = 0.0
 
         # Generate the threshold for each neuron
         # self.threshold = np.zeros(size, dtype=np.double) + 20.0
-        self.threshold_potential = 15 * np.random.rand(size) + 15.0
+        self.threshold_potential = 60.0
 
         # Generate the resting potential of each neuron
         # self.resting_potential = np.zeros(size, dtype=np.double)
-        self.resting_potential = -5 * np.random.rand(size) - 5
+        self.resting_potential = 5
 
         # Set the decay rate of each neuron
-        self.decay = 2 * np.random.rand(size)
+        self.decay = 0.5
 
-        self.axon = np.array([[0.0, 1.0, 0.0], [0.0, 0.0, 1.0], [0.0, 0.0, 0.0]])
+        # self.axon = np.array([[0.0, 1.0, 0.0], [0.0, 0.0, 1.0], [0.0, 0.0, 0.0]])
 
-        self.activation = []
+        self.activation = 0.0
         # for x in range(0, 100):
         #     self.activation.append(np.zeros(size, dtype=np.double))
 
         self.history = [0.0]
 
-        self.data = []
-        self.t = 0
+        self.activation_history = [0.0]
+        self.mp = [0.0]
+        self.t = [0.0]
+
+        self.time_step = 0.1
 
     def state(self):
         return [self.t, self.neuron, self.resting_potential, self.threshold_potential, self.decay, self.axon, self.activation]
 
     def activate(self, input):
-        self.activation = (np.sum(self.axon * self.neuron, axis=0))
-        self.activation[0:self.input_neurons] += input
+        # self.activation = (np.sum(self.axon * self.neuron, axis=0))
+        self.activation += input
+        self.activation_history.append(input)
 
     def step(self, input):
         self.activate(input)
 
-        x = self.neuron[:, 0]
+        x = self.neuron
 
         # decay
         # The decay term to bring the potential back towards resting potential
@@ -69,14 +73,9 @@ class Network:
                     4 / (1 + np.exp(-10 * (x - self.threshold_potential * 1.05))))
 
         # Calculate the final potential of each neuron at this time step
-        self.neuron[:, 0] = s + p * self.activation
+        self.neuron = s + p * self.activation
 
-        # Add the data from this time step
-        N = 0
-        for nv in self.neuron:
-            self.data.append([N, self.t, nv[0]])
-            N += 1
-
-        self.t += 1
+        self.mp.append(s + p * self.activation)
+        self.t.append(self.t[-1] + self.time_step)
 
         return self.neuron
--- spike.py
+++ spike.py
@@ -1,16 +1,36 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import time
-
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.collections import PolyCollection
-from matplotlib import colors as mcolors
 
 import network
 import numpy as np
 import math
 
 
+simulation_speed = 10
+
+fig = plt.figure()
+fig.subplots_adjust(wspace=1.0, hspace=1.0)
+# oa_subplot = fig.add_subplot(2, 2, 2)
+mp_subplot = fig.add_subplot(1, 1, 1)
+# na_subplot = fig.add_subplot(2, 2, 3)
+# k_subplot = fig.add_subplot(2, 2, 4)
+nn = network.Network()
+
+window = 200
+
+oa = []
+mp = []
+na = []
+k = []
+inputs = []
+
+
 def time_trial():
+    """
+    Run the simulation and measure the amount of time it takes to run each step
+    :return:
+    """
     x = []
     times = []
 
@@ -42,50 +62,44 @@ def time_trial():
     plt.show()
 
 
+def simulate(i):
+    input = 0.0
+
+    if 50 < i < 60:
+        input = 0.0
+
+    inputs.append(input)
+    if len(inputs) >= window:
+        inputs.pop(0)
+
+    nn.step(input)
+
+
+def get_window(x):
+    return x[-window:window]
+
+
+def mp_graph():
+    xs = nn.t
+    ys = nn.mp
+    mp_subplot.clear()
+    mp_subplot.plot(xs, ys)
+    mp_subplot.plot(xs, nn.activation_history)
+    mp_subplot.set_ylabel('membrane potential (mV)')
+    mp_subplot.set_ylim(-80, 50)
+
+
+def animate(i):
+    for x in range(0, 1):
+        simulate(i)
+    mp_graph()
+
+
 if __name__ == '__main__':
     np.set_printoptions(precision=2)
 
-    nn = network.Network()
+    print(nn.t, nn.activation_history)
 
-    timesteps = []
-    activations = []
-    for t in range(0, 1000):
-        activation = 10 if math.sin(t / 10) > 0.99 else 0
-        input = np.array([activation])
-        nn.step(input)
-        activations.append(activation)
-        timesteps.append(t)
-
-    xyz = np.array(nn.data)
-
-    fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    ax = fig.add_subplot(111)
-
-    # Plot the input signal
-    ax.plot(timesteps, activations)
-
-    N = 0
-    for N in range(0, len(nn.neuron)):
-        xyz2 = []
-        for point in xyz:
-            if point[0] == N:
-                xyz2.append(point)
-
-        xyz2 = np.array(xyz2)
-        # Voltage
-        z = xyz2[:, 2]
-        # Time
-        y = xyz2[:, 1]
-        # Neuron
-        x = xyz2[:, 0]
-
-        # ax.plot(x, y, z)
-        ax.plot(y, z)
-
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Potential(mV)')
-    # ax.set_zlabel('POTENTIAL')
-    plt.grid()
+    ani = animation.FuncAnimation(fig, animate, interval=1)
 
     plt.show()
